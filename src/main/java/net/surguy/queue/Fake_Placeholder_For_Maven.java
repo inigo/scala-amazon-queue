1eb0dc7b3d511d6a3a0177893bb7d92f11515aba
Inigo <inigo.surguy@gmail.com>
2013-06-30 18:33:27 +0100

    Created a fake Java class for the Sonatype release process to Maven Central

---
+++ src/main/java/net/surguy/queue/Fake_Placeholder_For_Maven.java
@@ -0,0 +1,10 @@
+package net.surguy.queue;
+
+/**
+ * Placeholder to make the Sonatype upload to Maven Central happy.
+ *
+ * @author Inigo Surguy
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class Fake_Placeholder_For_Maven {
+}
