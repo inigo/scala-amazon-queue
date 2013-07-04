c64a351a579e5d762c3d56c70d8960128e104663
Inigo <inigo.surguy@gmail.com>
2013-07-04 10:54:12 +0100

    Added log messages

--- src/main/scala/net/surguy/queue/QueuePreparer.scala
+++ src/main/scala/net/surguy/queue/QueuePreparer.scala
@@ -11,20 +11,25 @@ import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
 import java.util.Random
 import java.util
+import org.slf4j.LoggerFactory
 
 /**
  * Uploads a directory of local files to a queue and an associated file store.
  */
-class QueuePreparer(val store: Store, val queue: Queue[_]) {
-  def transferToStore(localDirectory: File) { localDirectory.listFiles().foreach( f => store.addToStore(f.getName, f) ) }
+class QueuePreparer(val store: Store, val queue: Queue[_]) extends Logging {
+  def transferToStore(localDirectory: File) {
+    log.debug("Transferring contents of local directory "+localDirectory+" to file store")
+    localDirectory.listFiles().foreach( f => store.addToStore(f.getName, f) )
+  }
   def populateQueue() { store.listContents().foreach( queue.add ) }
 }
 
 /**
  * Processes each of the items in a queue, retrieving each from a store by its identifier and processing it.
  */
-class QueueConsumer[U](val store: Store, val queue: Queue[U]) {
+class QueueConsumer[U](val store: Store, val queue: Queue[U]) extends Logging {
   def processNextIdentifier[T](fn: ((String, InputStream) => T)) = queue.nextIdentifier().map{ m =>
+    log.debug("Processing new identifier from queue: "+m.identifier)
     val result = fn(m.identifier, store.retrieveFromStore(m.identifier))
     queue.removeMessage(m)
     result
@@ -46,13 +51,14 @@ abstract class Queue[T] {
   def countMessages: Int
 }
 
-class AmazonS3Store(val bucketName: String) extends Store {
+class AmazonS3Store(val bucketName: String) extends Store with Logging {
   private val client = {
     val s3 = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider())
     s3.setRegion(Region.getRegion(Regions.US_WEST_2))
     if (!s3.listBuckets().map(_.getName).contains(bucketName)) {
       s3.createBucket(bucketName)
     }
+    log.debug("Starting Amazon S3 store with bucket "+bucketName)
     s3
   }
 
@@ -64,14 +70,24 @@ class AmazonS3Store(val bucketName: String) extends Store {
       objects = client.listNextBatchOfObjects(objects)
       contents.addAll(objects.getObjectSummaries.map( _.getKey ))
     }
+    log.debug("Listing "+contents.length+" items in bucket "+bucketName)
     contents.toList
   }
-  def addToStore(identifier: String, file: File) { client.putObject(new PutObjectRequest(bucketName, identifier, file)) }
-  def retrieveFromStore(identifier: String) = client.getObject(new GetObjectRequest(bucketName, identifier)).getObjectContent
-  def clearAll() { client.deleteObjects(new DeleteObjectsRequest(bucketName).withKeys( listContents(): _*  )) }
+  def addToStore(identifier: String, file: File) {
+    log.debug("Uploading "+identifier+" to bucket "+bucketName)
+    client.putObject(new PutObjectRequest(bucketName, identifier, file))
+  }
+  def retrieveFromStore(identifier: String) = {
+    log.debug("Downloading "+identifier+" from bucket "+bucketName)
+    client.getObject(new GetObjectRequest(bucketName, identifier)).getObjectContent
+  }
+  def clearAll() {
+    log.debug("Deleting all objects from bucket "+bucketName)
+    client.deleteObjects(new DeleteObjectsRequest(bucketName).withKeys( listContents(): _*  ))
+  }
 }
 
-class AmazonSqsQueue(val queueName: String) extends Queue[String] {
+class AmazonSqsQueue(val queueName: String) extends Queue[String] with Logging {
   private val client = {
     val sqs = new AmazonSQSClient(new ClasspathPropertiesFileCredentialsProvider())
     sqs.setRegion(Region.getRegion(Regions.US_WEST_2))
@@ -82,26 +98,37 @@ class AmazonSqsQueue(val queueName: String) extends Queue[String] {
     // This determines how long a message is hidden from other requesters after it's been received (seconds)
     // It should be similar to the expected time taken to process a message
     createRequest.setAttributes(Map(QueueAttributeName.VisibilityTimeout.name() -> "120"))
+    log.debug("Starting Amazon SQS queue with name "+queueName)
     client.createQueue(createRequest).getQueueUrl
   }
 
-  def add(identifier: String) { client.sendMessage(new SendMessageRequest(queueUrl, identifier)) }
+  def add(identifier: String) {
+    log.debug(s"Adding a new identifier $identifier to queue $queueName")
+    client.sendMessage(new SendMessageRequest(queueUrl, identifier))
+  }
   def nextIdentifier() = {
+    log.debug(s"Checking $queueName for a new message")
     val receiveMessageRequest = new ReceiveMessageRequest(queueUrl)
     receiveMessageRequest.setMaxNumberOfMessages(1) // How many messages to retrieve at once
     receiveMessageRequest.setWaitTimeSeconds(20) // How long to block waiting for a message to become available (20 is maximum)
     val messages = client.receiveMessage(receiveMessageRequest).getMessages
+    if (messages.isEmpty) log.debug("No message returned from queue "+queueName)
     messages.headOption.map( m => new MessageWrapper(m.getBody, m.getReceiptHandle) )
   }
-  def removeMessage(msg: MessageWrapper[String]) { client.deleteMessage(new DeleteMessageRequest(queueUrl, msg.messageHandle)) }
+  def removeMessage(msg: MessageWrapper[String]) {
+    log.debug("Removing message "+msg.identifier+" from queue "+queueName)
+    client.deleteMessage(new DeleteMessageRequest(queueUrl, msg.messageHandle))
+  }
 
   def countMessages = {
     val request = new GetQueueAttributesRequest(queueUrl)
     request.setAttributeNames(List(QueueAttributeName.ApproximateNumberOfMessages.name,
       QueueAttributeName.ApproximateNumberOfMessagesNotVisible.name))
     val attributes = client.getQueueAttributes(request).getAttributes
-    attributes(QueueAttributeName.ApproximateNumberOfMessages.name).toInt +
-      attributes(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.name).toInt
+    val visibleMessages = attributes(QueueAttributeName.ApproximateNumberOfMessages.name).toInt
+    val hiddenMessages = attributes(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.name).toInt
+    log.debug(s"Queue $queueName contains approximately $visibleMessages visible and $hiddenMessages hidden messages")
+    visibleMessages + hiddenMessages
   }
 
   def clearAll() {
@@ -122,3 +149,6 @@ class AmazonSqsQueue(val queueName: String) extends Queue[String] {
 }
 case class MessageWrapper[T](identifier: String, messageHandle: T)
 
+private[queue] trait Logging {
+  protected lazy val log = LoggerFactory.getLogger(this.getClass)
+}
